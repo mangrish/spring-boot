ae8c5c587f711843b30d15b8fea3e1cc7db425ac
Mark Angrish <mark.angrish@gmail.com>
2016-07-27 00:10:22 +1000

    Fixed broken generated test

--- spring-boot-samples/spring-boot-sample-data-neo4j/src/main/java/sample/data/neo4j/CustomerRepository.java
+++ spring-boot-samples/spring-boot-sample-data-neo4j/src/main/java/sample/data/neo4j/CustomerRepository.java
@@ -18,6 +18,9 @@ package sample.data.neo4j;
 
 import java.util.List;
 
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+
 public interface CustomerRepository extends Neo4jRepository<Customer> {
 
 	public Customer findByFirstName(String firstName);
